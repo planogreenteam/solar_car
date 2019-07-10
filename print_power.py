1a7a14e7890d24de908b1d9757af5136bdad0d9b
kvenkatesh5 <happyvkv@gmail.com>
2019-07-10 18:38:05 -0500

    HTML

--- print_power.py
+++ print_power.py
@@ -1,27 +1,46 @@
 import time
-import linecache
 
 
-def line_jump():
-    line_offset = []
-    offset = 0
-    currentfile = open('currentdata.txt', 'r')
-    currentfile.close()
+path = "/var/www/html/index.html"
+HTML_file = open(path, "w")
 j = 0
-while True: 
+while True:
 
     time.sleep(2)
     currentfile = open('currentdata.txt', 'r')
-    lines = currentfile.readlines() 
+    lines = currentfile.readlines()
     line = lines[j]
     print(line)
     currentfile.close()
+
     voltfile = open('voltagedata.txt', 'r')
     linesv = voltfile.readlines()
     linev = linesv[j]
     print(linev)
+    voltfile.close()
+
     intc = int(line)
     intv = int(linev)
     power = intc * intv
     print("power:", power)
     j += 1
+
+    html_str = '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title> Monitor</title>
+        </head>
+        <body>
+
+        <h1> Power Out: <%= power%> </h1>
+        <h1> Main Battery Voltage: <%= linev %> </h1>
+
+        </body>
+        </html>
+    '''
+
+    HTML_file.write(html_str)
+
+
+HTML_file.close()
